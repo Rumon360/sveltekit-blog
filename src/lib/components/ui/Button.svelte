2cd3eaf2fbbc064de032441a65a084411fd35659
Rumon360 <hmk.rumon@gmail.com>
2025-12-30 22:31:36 +0600

    feat: button component added

---
+++ src/lib/components/ui/Button.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let type: 'button' | 'submit' | 'reset' = 'button';
+	export let disabled: boolean = false;
+</script>
+
+<button
+	{type}
+	{disabled}
+	class="
+		inline-flex items-center justify-center
+		bg-zinc-100
+		px-4 py-2
+		text-sm font-medium
+		text-zinc-900
+		disabled:cursor-not-allowed
+		disabled:opacity-50
+	"
+>
+	<slot />
+</button>
