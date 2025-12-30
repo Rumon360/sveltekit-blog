4ec7da78567d27ae807a3fe2dc45e604c1b333eb
Rumon360 <hmk.rumon@gmail.com>
2025-12-31 01:39:55 +0600

    chore: update button

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
+
 	export let type: 'button' | 'submit' | 'reset' = 'button';
-	export let disabled: boolean = false;
+	export let disabled = false;
+
+	$: className = $$restProps.class || '';
 </script>
 
 <button
 	{type}
 	{disabled}
-	class="
-		inline-flex items-center justify-center
-		bg-zinc-100
-		px-4 py-2
-		text-sm font-medium
-		text-zinc-900
-		disabled:cursor-not-allowed
-		disabled:opacity-50
-	"
+	{...$$restProps}
+	class={cn(
+		'inline-flex items-center justify-center bg-zinc-100 px-4 py-2 text-sm font-medium text-zinc-900 disabled:cursor-not-allowed disabled:opacity-50',
+		className
+	)}
 >
 	<slot />
 </button>
