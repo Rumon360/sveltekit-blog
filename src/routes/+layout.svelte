a1c66ebcb13980ca9b0f38d93070731fbffd6970
Rumon360 <hmk.rumon@gmail.com>
2025-12-31 02:07:00 +0600

    feat: navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,22 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { authClient } from '$lib/auth-client.js';
+	import { goto, invalidate } from '$app/navigation';
+	import Button from '$lib/components/ui/Button.svelte';
 
-	let { children } = $props();
+	let { children, data } = $props();
+
+	const handleLogout = async () => {
+		await authClient.signOut({
+			fetchOptions: {
+				onSuccess: async () => {
+					await invalidate('auth:status');
+					goto('/');
+				}
+			}
+		});
+	};
 </script>
 
 <svelte:head>
@@ -11,5 +25,13 @@
 </svelte:head>
 
 <main class="container mx-auto w-full px-4 lg:px-0">
+	<nav class="flex h-16 w-full items-center justify-between border-b border-zinc-700">
+		<a href="/" class="inline-block text-2xl font-bold">SvelteKit Blog</a>
+		{#if data.isAuthenticated}
+			<Button onclick={handleLogout} class="border border-zinc-300 bg-transparent text-zinc-50">
+				Logout
+			</Button>
+		{/if}
+	</nav>
 	{@render children()}
 </main>
