d23f139ab4706679ab0b9c7a00962bffcbbcd784
Rumon360 <hmk.rumon@gmail.com>
2025-12-31 01:08:25 +0600

    feat: better auth middleware and auth gaurd

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import type { Handle } from '@sveltejs/kit';
+import { auth } from '$lib/auth';
+import { svelteKitHandler } from 'better-auth/svelte-kit';
+import { building } from '$app/environment';
+import { sequence } from '@sveltejs/kit/hooks';
+
+export const authHandle: Handle = async ({ event, resolve }) => {
+	return svelteKitHandler({ event, resolve, auth, building });
+};
+
+const sessionHandle: Handle = async ({ event, resolve }) => {
+	const session = await auth.api.getSession({ headers: event.request.headers });
+	event.locals.user = session?.user;
+	const response = await resolve(event);
+	return response;
+};
+
+export const handle = sequence(authHandle, sessionHandle);
