53538f74ffa49257d589caa4f515e9e1c10802c0
Rumon360 <hmk.rumon@gmail.com>
2025-12-30 22:31:43 +0600

    feat: input component

---
+++ src/lib/components/ui/Input.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let id: string;
+	export let name: string;
+	export let type: string = 'text';
+	export let value: string = '';
+</script>
+
+<input
+	bind:value
+	{id}
+	{name}
+	{type}
+	class="
+		w-full
+		border border-zinc-700
+		bg-zinc-900
+		px-3 py-2
+		text-sm text-zinc-100
+		transition
+		outline-none
+		placeholder:text-zinc-500
+		focus:border-zinc-500
+		focus:ring-1 focus:ring-zinc-500
+	"
+	{...$$restProps}
+/>
