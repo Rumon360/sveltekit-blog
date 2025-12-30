8c5e0d010b1306aa6def9530324a02e9a04b1e48
Rumon360 <hmk.rumon@gmail.com>
2025-12-30 22:32:21 +0600

    feat: auth layout

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Auth - SvelteKit Blog</title>
+</svelte:head>
+
+<div class="flex min-h-screen w-full items-center justify-center">
+	{@render children()}
+</div>
