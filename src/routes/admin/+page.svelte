eee31a67350e15ec867dd89df099c49483ac1397
Rumon360 <hmk.rumon@gmail.com>
2026-01-03 22:56:39 +0600

    chore: update route

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
+	import Button from '$lib/components/ui/Button.svelte';
 	import { get_all_posts } from './posts.remote';
 
 	const posts = await get_all_posts();
 </script>
 
 <div class="pt-12">
-	<div class="mb-12">
-		<h1 class="text-3xl font-bold tracking-tight">Welcome back</h1>
-		<p class="mt-2 text-zinc-400">Here’s all of the blog posts.</p>
+	<div class="mb-12 flex w-full flex-col justify-between gap-6 sm:flex-row">
+		<div>
+			<h1 class="text-3xl font-bold tracking-tight">Welcome back</h1>
+			<p class="mt-2 text-zinc-400">Here’s all of the blog posts.</p>
+		</div>
+		<div>
+			<Button>
+				<a href={resolve('/admin/post/new')}>New Post</a>
+			</Button>
+		</div>
 	</div>
 	<ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
 		{#each posts as post (post.id)}
 			<li>
 				<a
-					href={`/posts/${post.slug}`}
+					href={`/admin/post/${post.slug}`}
 					class="group block border border-zinc-800 bg-zinc-900/60 p-6 transition
 						hover:border-zinc-700 hover:bg-zinc-900"
 				>
