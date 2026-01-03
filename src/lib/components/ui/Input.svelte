c92171e72b15f60107bb16cd0bc6c9a31d2903cc
Rumon360 <hmk.rumon@gmail.com>
2026-01-03 22:56:20 +0600

    chore: use input utility class

--- src/lib/components/ui/Input.svelte
+++ src/lib/components/ui/Input.svelte
@@ -5,22 +5,4 @@
 	export let value: string = '';
 </script>
 
-<input
-	bind:value
-	{id}
-	{name}
-	{type}
-	class="
-		w-full
-		border border-zinc-700
-		bg-zinc-900
-		px-3 py-2
-		text-sm text-zinc-100
-		transition
-		outline-none
-		placeholder:text-zinc-500
-		focus:border-zinc-500
-		focus:ring-1 focus:ring-zinc-500
-	"
-	{...$$restProps}
-/>
+<input bind:value {id} {name} {type} class="input-field" {...$$restProps} />
